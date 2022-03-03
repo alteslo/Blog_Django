10908f6606f9986f261a2fe1f6a3790be523c891
Teslenko A.A.(work) <alteslo31@gmail.com>
2022-03-03 18:09:07 +0300

    add main structure Django and base view

--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+
+class MainView(View):
+    template_name = 'myblog\index.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
