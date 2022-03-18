941684704e3b27dd3f162335d2c17ddedeaa410a
Teslenko.A.A <alteslo31@gmail.ru>
2022-03-18 12:59:18 +0300

    some changes

--- blog/myblog/urls.py
+++ blog/myblog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.MainView.as_view(), name='index'),
+    path('blog/<slug>/', views.PostDetailView.as_view(), name='post_detail'),
 ]
--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
+from .models import Post
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 
 class MainView(View):
-    template_name = 'myblog\home.html'
+    template_name = 'myblog/home.html'
+    post = Post
+    paginator = Paginator
 
     def get(self, request):
-        return render(request, self.template_name)
+        posts = self.post.objects.all()
+        paginator = self.paginator(posts, 6)
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        context = {'page_obj': page_obj}
+        return render(request, self.template_name, context)
+
+
+class PostDetailView(View):
+    template = 'myblog/post_detail.html'
+
+    def get(self, request, slug):
+        post = get_object_or_404(Post, url=slug)
+        context = {'post': post}
+        return render(request, self.template, context)
